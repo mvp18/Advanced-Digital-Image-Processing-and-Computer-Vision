6994b554020235703787ed9a82244c45285f8097
mvp18 <soumava2016@gmail.com>
2020-03-09 01:33:50 +0530

    Assgn 3 third part

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import argparse
 from modules import *
+from utils import *
 
 def main(args):
 
@@ -12,6 +13,10 @@ def main(args):
 		X1, X2 = compute_SURF_ckp(img1, img2, args.save_flag)	
 	else:
 		X1, X2 = compute_SIFT_ckp(img1, img2, args.save_flag)
+
+	fundamental_matrix = solve_8pt_corr(X1, X2)
+
+	draw_epipolar_lines(X1, X2, fundamental_matrix, img1, img2)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Implementation of tasks in Assignment 3.")
---
+++ Assignment 3/utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+
+def solve_8pt_corr(X1, X2):
+
+	A = np.array([[X1[i][0]*X2[i][0], X2[i][0]*X1[i][1], X2[i][0], X1[i][0]*X2[i][1], 
+				   X1[i][1]*X2[i][1], X2[i][1], X1[i][0], X1[i][1]] for i in range(len(X1))])
+	f = np.ones([8, 1])*(-1)
+	F = np.matmul(np.linalg.inv(A), f)
+	F = np.append(F, 1)
+	F = F.reshape(3, 3)
+	U, D, VT = np.linalg.svd(F)
+	D_ = np.array([[D[0], 0, 0],
+				   [D[1], 0, 0],
+				   [0, 0, 0]])
+
+	F_ = np.matmul(U, np.matmul(D_, VT))
+
+	return F_
+
+def draw_epipolar_lines(X1, X2, F, img1, img2):
+
+	L = []
+	L_ = []
+	img3 = img1
+	img4 = img1
+	
+	for i in range(len(X1)):
+		x = np.array([X1[i][0], X1[i][1], 1]).reshape(3, 1)
+		x_ = np.array([X2[i][0], X2[i][1], 1]).reshape(3, 1)
+		l = np.matmul(F.transpose(), x_)
+		l_ = np.matmul(F, x)
+		L.append(l)
+		L_.append(l_)
+		print(int(-l[2]/l[0]))
+		print(int(-l[2]/l[1]))
+		print(int(-l_[2]/l_[0]))
+		print(int(-l_[2]/l_[1]))
+		img3 = cv2.line(img3, (0, int(-l[2]/l[0])), (int(-l[2]/l[1]), 0), (0, 0, 255), 2)
+		img4 = cv2.line(img4, (0, int(-l_[2]/l_[0])), (int(-l_[2]/l_[1]), 0), (0, 255, 0), 2)
+
+	cv2.imshow('Epipolar_lines1', img3)
+	cv2.imshow('Epipolar_lines2', img4)
+	
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
